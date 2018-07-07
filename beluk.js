aed989b8a11cdd1c93f257d1e350315e01455f0c
donasiku <32330665+donasiku@users.noreply.github.com>
2018-07-07 18:23:21 +0700

    Update beluk.js

--- beluk.js
+++ beluk.js
@@ -21,21 +21,62 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*15)+ 1);
 var xxx=null;
-if(x=="1"){xxx="what-is-bitcoin-mining-pool.html"}
-if(x=="2"){xxx="newsbitcoincom-is-hiring-editorial.html"}
-if(x=="3"){xxx="boomstarternetwork-to-democratize.html"}
-if(x=="4"){xxx="vertex-launches-ico-aftermarket.html"}
-if(x=="5"){xxx="decentralized-crowdfunding-platform.html"}
-if(x=="6"){xxx="switzerland-considers-granting-crypto.html"}
-if(x=="7"){xxx="thailand-officially-announces-rules-for.html"}
-if(x=="8"){xxx="quoines-ico-platform-adds-bch-as.html"}
-if(x=="9"){xxx="global-data-report-cryptocurrencies-are.html"}
-if(x=="10"){xxx="the-daily-bittrex-invades-europe-and.html"}
-if(x=="11"){xxx="quebec-mining-drama-bitmain-money.html"}
-if(x=="12"){xxx="heavy-rainfall-in-china-thousands-of.html"}
-if(x=="13"){xxx="japanese-man-exploits-free-company.html"}
-if(x=="14"){xxx="what-is-bitcoin.html"}
-if(x=="15"){xxx="predicting-bitcoin-to-go-to-zero-will.html"}a_to_ve[i].href="https://abubosok.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2018/07/what-is-bitcoin-mining-pool.html"}
+if(x=="2"){xxx="2018/07/newsbitcoincom-is-hiring-editorial.html"}
+if(x=="3"){xxx="2018/07/boomstarternetwork-to-democratize.html"}
+if(x=="4"){xxx="2018/07/vertex-launches-ico-aftermarket.html"}
+if(x=="5"){xxx="2018/07/decentralized-crowdfunding-platform.html"}
+if(x=="6"){xxx="2018/07/switzerland-considers-granting-crypto.html"}
+if(x=="7"){xxx="2018/07/thailand-officially-announces-rules-for.html"}
+if(x=="8"){xxx="2018/07/quoines-ico-platform-adds-bch-as.html"}
+if(x=="9"){xxx="2018/07/global-data-report-cryptocurrencies-are.html"}
+if(x=="10"){xxx="2018/07/the-daily-bittrex-invades-europe-and.html"}
+if(x=="11"){xxx="2018/07/quebec-mining-drama-bitmain-money.html"}
+if(x=="12"){xxx="2018/07/heavy-rainfall-in-china-thousands-of.html"}
+if(x=="13"){xxx="2018/07/japanese-man-exploits-free-company.html"}
+if(x=="14"){xxx="2018/07/what-is-bitcoin.html"}
+if(x=="15"){xxx="2018/07/predicting-bitcoin-to-go-to-zero-will.html"}
+if(x=="16"){xxx="2018/07/aplikasi-full-versi.html"}
+if(x=="17"){xxx="2018/07/download-filem-terbaru.html"}
+if(x=="18"){xxx="2018/07/download-aplikasi.html"}
+if(x=="19"){xxx="2018/07/aplikasi-pulsa.html"}
+if(x=="20"){xxx="2018/07/bisnis-pulsa-dan-ppob.html"}
+if(x=="21"){xxx="2018/07/server-pulsa-free.html"}
+if(x=="22"){xxx="2018/07/facebook-akan-tutup.html"}
+if(x=="23"){xxx="2018/07/youtube-akan-tutup.html"}
+if(x=="24"){xxx="2018/07/filem-terbaru.html"}
+if(x=="25"){xxx="2018/07/era-uang-digital.html"}
+if(x=="26"){xxx="2018/07/uang-digita.html"}
+if(x=="27"){xxx="2018/07/kopi-terbaik-dunia.html"}
+if(x=="28"){xxx="2018/07/os-paling-baik.html"}
+if(x=="29"){xxx="2018/07/bank-teraman-didunia.html"}
+if(x=="30"){xxx="2018/07/browser-terbaik.html"}
+if(x=="31"){xxx="2018/07/download-idm-terbaru.html"}
+if(x=="32"){xxx="2018/07/hp-terbaik-sepanjang-masa.html"}
+if(x=="33"){xxx="2018/07/virus-mematikan.html"}
+if(x=="34"){xxx="2018/07/script-hacker.html"}
+if(x=="35"){xxx="2018/07/cek-antivirus.html"}
+if(x=="36"){xxx="2018/07/one-minute-solutions-to-your-improving.html"}
+if(x=="37"){xxx="2018/07/insurance-across-world.html"}
+if(x=="38"){xxx="2018/07/make-money-in-forex-for-beginners.html"}
+if(x=="39"){xxx="2018/07/web-hosting-service.html"}
+if(x=="40"){xxx="2018/07/best-forex-strategies.html"}
+if(x=="41"){xxx="2018/07/best-new-cars-for-2020.html"}
+if(x=="42"){xxx="2018/07/best-smartphones-and-mobile-phones.html"}
+if(x=="43"){xxx="2018/07/best-antivirus-for-android.html"}
+if(x=="44"){xxx="2018/07/bank-dengan-aset-terbesar.html"}
+if(x=="45"){xxx="2018/07/binary-options-explained-simplified.html"}
+if(x=="46"){xxx="2018/07/windows-release-date-price-news.html"}
+if(x=="47"){xxx="2018/07/best-motorcycle-insurance-quotes-rates.html"}
+if(x=="48"){xxx="2018/07/perusahaan-asuransi-terbaik.html"}
+if(x=="49"){xxx="2018/07/how-to-make-money.html"}
+if(x=="50"){xxx="2018/07/study-abroad-scholarship-in-japan.html"}
+if(x=="51"){xxx="2018/07/ways-to-get-cheapest-health-insuranc.html"}
+if(x=="52"){xxx="2018/07/auto-insurance-quotes.html"}
+if(x=="53"){xxx="2018/07/how-to-read-forex-chart-properly.html"}
+if(x=="54"){xxx="2018/07/google-drive-cloud.html"}
+if(x=="55"){xxx="2018/07/stress-eating-helps.html"}
+if(x=="56"){xxx="2018/07/best-auto-insurance-in-indonesia.html"}a_to_ve[i].href="https://abubosok.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
